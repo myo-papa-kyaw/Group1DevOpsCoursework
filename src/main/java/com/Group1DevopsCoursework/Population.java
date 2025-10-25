6ef268981a9c36d76cb9293837af805902881984
Thinzar-Phyo-Wai <40804632@live.napier.ac.uk>
2025-10-25 15:15:46 +0630

    update files

---
+++ src/main/java/com/Group1DevopsCoursework/Population.java
@@ -0,0 +1,10 @@
+package com.Group1DevopsCoursework;
+
+public class Population {
+    public String name;
+    public long totalPopulation;
+    public long cityPopulation;
+    public long nonCityPopulation;
+    public double cityPercentage;
+    public double nonCityPercentage;
+}
