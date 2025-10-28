4b17dcdcafee6670da757b9b5954cc00c6fb08b8
myo-papa-kyaw <40804635@live.napier.ac.uk>
2025-10-28 14:02:24 +0630

    Updated Files in my own branch

--- DevOps/Group1DevopsCoursework/src/main/java/com/Group1DevopsCoursework/City.java
+++
@@ -1,8 +0,0 @@
-package com.Group1DevopsCoursework;
-
-public class City {
-    public String name;
-    public String country;
-    public String district;
-    public int population;
-}
--- DevOps/Group1DevopsCoursework/src/main/java/com/Group1DevopsCoursework/Language.java
+++
@@ -1,7 +0,0 @@
-package com.Group1DevopsCoursework;
-
-public class Language {
-    public String language;
-    public long speakers;
-    public double percentage;
-}
