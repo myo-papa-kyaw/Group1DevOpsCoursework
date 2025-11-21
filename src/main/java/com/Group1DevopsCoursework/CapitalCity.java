013d87c0c2720d1fce897e367f15c4bf40ac8f6d
sandaraung35 <40794409@live.napier.ac.uk>
2025-11-21 23:43:42 +0630

    Update File

--- src/main/java/com/Group1DevopsCoursework/CapitalCity.java
+++ src/main/java/com/Group1DevopsCoursework/CapitalCity.java
@@ -1,9 +1,23 @@
 package com.Group1DevopsCoursework;
 
+/**
+ * Represents a capital city within the world database.
+ * Each CapitalCity object stores:
+ * - The name of the capital city
+ * - The country it belongs to
+ * - Its population
+ * This class is primarily used to support reporting queries that retrieve
+ * capital cities in various scopes (world, continent, region, filtered lists, etc.).
+ */
+
 public class CapitalCity {
 
-        public String name;
-        public String country;
-        public int population;
-    }
+    /** The name of the capital city. */
+    public String name;
 
+    /** The country to which the capital city belongs. */
+    public String country;
+
+    /** The population of the capital city. */
+    public int population;
+}
